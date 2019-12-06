9644c5c16798dd2a68662e62cf0a774a75b200e7
whddbs507 <whddbs5071@naver.com>
2019-12-06 17:42:55 +0900

    12/6(금)

--- bitcamp-java/src/main/java/com/ohoracs/basic/ex03/Exam1.java
+++ bitcamp-java/src/main/java/com/ohoracs/basic/ex03/Exam1.java
@@ -19,10 +19,17 @@ public class Exam1 {
     System.out.println(0b01001110);  // 2진수 표기법 (0b부터 시작하면 2진수)
     System.out.println(-0b01001110);  // 2진수 표기법
     // 부동소수점 리터럴
-
+    System.out.println(3.14);  // 고정소수점
+    System.out.println(0.314e1);   // 부동소수점 (0.314 * 10의1승)
+    System.out.println(31.4e-1);   // 부동소수점
     // 논리 리터럴
-
+    System.out.println(true);
+    System.out.println(false);
     // 문자 리터럴
+    System.out.println('A');
+    System.out.println('가');
 
+    // 문자열 리터럴
+    System.out.println("안녕하세요. 비트캠프!");
   }
 }
